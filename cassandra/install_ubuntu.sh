52b134c720ca06e5b4550b9660432dc8c230c2ed
javis-yan <yan_h@hotmail.com>
2018-06-09 23:59:57 +0800

    [infra]add cassandra/install_ubuntu.sh

---
+++ cassandra/install_ubuntu.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+###################
+#
+# Install Java
+#
+###################
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-set-default -y
+java -version
+
+###################
+#
+# Installing Cassandra
+#
+###################
+
+echo "deb http://www.apache.org/dist/cassandra/debian 22x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+echo "deb-src http://www.apache.org/dist/cassandra/debian 22x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+gpg --keyserver pgp.mit.edu --recv-keys F758CE318D77295D
+gpg --export --armor F758CE318D77295D | sudo apt-key add -
+gpg --keyserver pgp.mit.edu --recv-keys 2B5C1B00
+gpg --export --armor 2B5C1B00 | sudo apt-key add -
+pg --keyserver pgp.mit.edu --recv-keys 0353B12C
+gpg --export --armor 0353B12C | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install cassandra -y
